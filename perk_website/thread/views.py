baaf5798e6088880d06512bedbe565489533d5f7
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-08-09 16:35:24 +0100

    base html v1

--- perk_website/thread/views.py
+++ perk_website/thread/views.py
@@ -3,8 +3,42 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Steve K',
+        'title': 'First Blog Post',
+        'content': 'Content for the first post.',
+        'date_posted': '12/07/2023'
+    },
+
+     {
+        'author': 'Scott K',
+        'title': 'Second Blog Post',
+        'content': 'Content for the second post.',
+        'date_posted': '13/07/2023'
+    }
+
+]
 def home(request):
-    return HttpResponse('<h1>Thread Home</h1>')
-   
+   context = {
+        'posts': posts
+            }
+   return render(request, 'thread/home.html',context)
+
 def about(request):
-    return HttpResponse('<h1>Thread-About</h1>')
+    return render(request, 'thread/about.html',{'title': 'About'})
+   
+def league(request):
+    return render(request, 'thread/league.html')
+   
+def store(request):
+    return render(request, 'thread/store.html')
+   
+def thread(request):
+    return render(request, 'thread/thread.html')
+
+def contact(request):
+    return render(request, 'thread/contact.html')
+   
+
+
