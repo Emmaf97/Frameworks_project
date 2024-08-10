c85e8281e658bca069179e2f18080c2ea7e84602
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-08-10 11:12:27 +0100

    build config v1.5

--- perk_website/perk_website/settings.py
+++ perk_website/perk_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('https://frameworks-project.onrender.com/', '')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('frameworks-project.onrender.com', '')]
 
 
 # Application definition
