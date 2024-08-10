e65a1b8a74cc60318d72f6088c503ce014468355
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-08-10 13:24:41 +0100

    routes v1

--- perk_website/thread/urls.py
+++ perk_website/thread/urls.py
@@ -7,4 +7,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='thread-home'),
     path('about/',views.about, name='thread-about'),
+    path('league/',views.league, name='thread-league'),
+    path('store/',views.store, name='thread-store'),
+    path('thread/',views.thread, name='thread-thread'),
+    path('contact/',views.contact, name='thread-contact'),
 ]
