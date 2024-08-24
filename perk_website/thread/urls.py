ee7d6012da3427db34ef6f7ea61de83c6ea9bad3
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-08-24 20:51:25 +0100

    urls.py and views.py maps integration update v1.6

--- perk_website/thread/urls.py
+++ perk_website/thread/urls.py
@@ -16,12 +16,11 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='thread-home'),
-    # path('', views.home, name='thread-home'),
     path('about/',views.about, name='thread-about'),
     path('league/',views.league, name='thread-league'),
     path('store/',views.store, name='thread-store'),
+    path('maps/',views.maps, name='thread-maps'),
     path('thread/', PostListView.as_view(), name='thread-thread'),
-    # path('thread/',views.thread, name='thread-thread'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
--- perk_website/thread/views.py
+++ perk_website/thread/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Post, Contact
+from .models import Post, Contact, Maps
 from django.contrib import messages #import for messages
 from .forms import ContactForm
 from store.models import Products
@@ -66,9 +66,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): # New
        
                   
 def home(request):
-    # context = {
-    #    'posts': Post.objects.all()
-    # }
     return render(request, 'thread/home.html')
 
 def about(request):
@@ -76,50 +73,20 @@ def about(request):
    
 def league(request):
     return render(request, 'thread/league.html')
+
+def maps(request):
+    context ={
+        'maps': Maps.objects.all()
+    }
+    return render(request, 'thread/maps.html', context)
    
 def store(request):
-    # Load data from JSON file
-    json_file_path = os.path.join(settings.BASE_DIR, 'store', 'data', 'products.json')
-    # json_file_path = os.path.join(os.path.dirname(__file__), 'store', 'data', 'products.json')
+    context ={
+        'products': Products.objects.all()
+    }
+    return render(request, 'thread/store.html', context)
     
-    try:
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        return render(request, 'thread/store.html', {'error': 'File not found'})
-    except json.JSONDecodeError:
-        return render(request, 'thread/store.html', {'error': 'Error decoding JSON'})
     
-    # Debug: Print data to check its structure
-    print("Loaded data:", data)
-    
-    # Ensure data is a dictionary with a 'products' key
-    if isinstance(data, dict) and 'products' in data:
-        products_data = data['products']
-        if isinstance(products_data, list):
-            products = []
-            for item in products_data:
-                # Debug: Print each item to check its structure
-                print("Processing item:", item)
-                
-                if isinstance(item, dict):
-                    product = Products.objects.create(
-                        item_name=item.get('item_name'),
-                        description=item.get('description'),
-                        colour=item.get('colour'),
-                        quantity=item.get('quantity'),
-                        image=item.get('image')  # Ensure this path is correct
-                    )
-                    products.append(product)
-                else:
-                    return render(request, 'thread/store.html', {'error': 'Invalid data format in products list'})
-        else:
-            return render(request, 'thread/store.html', {'error': 'Invalid JSON format for products'})
-    else:
-        return render(request, 'thread/store.html', {'error': 'Invalid JSON structure'})
-    
-    return render(request, 'thread/store.html', {'products': products})
-   
 def thread(request):
     context = {
        'posts': Post.objects.all()
