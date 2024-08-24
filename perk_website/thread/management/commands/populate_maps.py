9c9ea4fd04597be1afce99fa639849b64fbfdf4c
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-08-24 20:57:59 +0100

    populate commands to dump json data

---
+++ perk_website/thread/management/commands/populate_maps.py
@@ -0,0 +1,34 @@
+import json
+from django.core.management.base import BaseCommand
+from thread.models import Maps
+
+class Command(BaseCommand):
+    help = 'Load maps from a JSON file into the database'
+
+    def handle(self, *args, **kwargs):
+        # Open the JSON file and load the data
+        with open('thread/data/maps.json', 'r') as file:
+            data = json.load(file)
+        
+        maps_data = data.get('zombie_maps', [])
+
+        for map_data in maps_data:
+            
+            existing_map = Maps.objects.filter(map_name=map_data['map_name']).first()
+
+            if existing_map:
+               
+                existing_map.delete()
+                print(f"Deleted existing product: {existing_map.map_name}")
+
+           
+            new_Map = Maps(
+                map_name=map_data['map_name'],
+                wonder_weapon=map_data['wonder_weapon'],
+                easter_egg=map_data['easter_egg'],
+                video_guide=map_data['video_guide'],
+            )
+            new_Map.save()
+            print(f"Created new product: {new_Map.map_name}")
+
+        print("Database updated: old entries deleted and new entries created.")
---
+++ perk_website/thread/management/commands/populate_store.py
@@ -0,0 +1,36 @@
+import json
+from django.core.management.base import BaseCommand
+from store.models import Products
+
+class Command(BaseCommand):
+    help = 'Load products from a JSON file into the database'
+
+    def handle(self, *args, **kwargs):
+        # Open the JSON file and load the data
+        with open('store/data/products.json', 'r') as file:
+            data = json.load(file)
+        
+        # Access the list of products within the "products" key
+        products_data = data.get('products', [])
+
+        for product_data in products_data:
+            # Check if the product already exists based on a unique field, such as 'item_name'
+            existing_product = Products.objects.filter(item_name=product_data['item_name']).first()
+
+            if existing_product:
+                # Delete the existing product
+                existing_product.delete()
+                print(f"Deleted existing product: {existing_product.item_name}")
+
+            # Now create a new entry
+            new_product = Products(
+                item_name=product_data['item_name'],
+                description=product_data['description'],
+                colour=product_data['colour'],
+                quantity=product_data['quantity'],
+                image=product_data['image']
+            )
+            new_product.save()
+            print(f"Created new product: {new_product.item_name}")
+
+        print("Database updated: old entries deleted and new entries created.")
